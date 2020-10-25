a56133c5f2138094298c41e15c03128414bab401
SonamT <stamang.mrbhutan90@gmail.com>
2020-10-26 01:20:47 +0600

    created login page and other components.

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// USER NAV DROPDOWN PANEL
+function showUserPanel() {
+    document.querySelector(".dropdown-panel").classList.toggle("dropdown-show");
+}
+
+document.querySelector('#dropdown-panel').addEventListener('click', function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  showUserPanel();
+});
+
+window.onclick = function(event) {
+  if (!event.target.matches('.user-nav__button')) {
+    let dropdowns = document.getElementsByClassName("dropdown-panel");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('dropdown-show')) {
+        openDropdown.classList.remove('dropdown-show');
+      }
+    }
+  }
+}
+
+document.getElementById('theme-change').addEventListener('click', function() {
+  document.querySelector('.theme__icon').toggle("ion-ios-moon");
+  document.querySelector('.theme__name').innerHTML = "<span class='theme__name'>Dark Theme</span>";
+});
+
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+ if (localStorage.getItem('theme') === 'theme-dark'){
+     setTheme('theme-light');
+ } else {
+     setTheme('theme-dark');
+ }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+ if (localStorage.getItem('theme') === 'theme-dark') {
+     setTheme('theme-dark');
+ } else {
+     setTheme('theme-light');
+ }
+})();
+
