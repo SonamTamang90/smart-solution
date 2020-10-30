d1888f2d95b745b61203e8647d39eac00193cf5d
SonamT <sonam.tamang.mr@gmail.com>
2020-10-30 09:28:25 +0600

    linking of pages

--- js/routing.js
+++ js/routing.js
@@ -1,5 +1,17 @@
+
+
+document.querySelector('.dropdown-panel--signout').addEventListener('click', function() {
+    window.location.href = "login.html";
+});
+
+
+
 /////////////////////////////////////////
 // NAVIGATING TO PAGES
 document.getElementById('bed-room').addEventListener('click', function() {
     window.location.href = "room.html";
 });
+
+
+
+
